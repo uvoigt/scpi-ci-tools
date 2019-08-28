6488fe548fe003b1b508d63d0e76c9ffc67a340d
Uwe Voigt <uwe.voigt@smartcommerce.de>
2019-08-28 13:09:43 +0200

    besser steuerbar

--- clone.sh
+++ clone.sh
@@ -6,25 +6,42 @@
 # ansonsten wird der Verzeichnisname aus dem Artefaktnamen abgeleitet.
 ########################################################################################################################
 
-. ./getSessionToken.sh
+. ./configure.sh
 
-ARTIFACT_ID="Submit_Order"
 ARTIFACT_VERSION="active" #1.0.0
 
-ZIP_FILE_NAME="$ARTIFACT_ID_$ARTIFACT_VERSION.zip"
-
 if [ "$1" != "" ]; then
-  FOLDER=$1
+  ARTIFACT_ID=$1
+  if [ "$2" != "" ]; then
+    FOLDER=$2
+  else
+    FOLDER="${ARTIFACT_ID}"
+    if [ "$3" != "" ]; then
+      ARTIFACT_VERSION=$3
+    fi
+  fi
 else
-  FOLDER="${ARTIFACT_ID}"
+  printf "Usage: %s [options] <artifact_id> [target_folder, default=artifact_id] [version, default=active]\n" "$0" 1>&2
+  print_options
+  printf "Please specifiy the artifact to clone.\n" 1>&2
+  exit 1
 fi
 
-echo "Downloading artifact $ARTIFACT_ID to folder $FOLDER"
-curl -sL \
+. ./getSessionToken.sh
+
+ZIP_FILE_NAME="$ARTIFACT_ID"_"$ARTIFACT_VERSION.zip"
+
+printf "Downloading artifact %s from account %s to folder %s\n" "$ARTIFACT_ID" "$ACCOUNT_ID" "$FOLDER" 1>&2
+RESPONSE=$(curl -sL \
      -H X-CSRF-Token:$XCSRFTOKEN \
      -u $USER:$PASSWORD \
-     --output "$ZIP_FILE_NAME" \
-     "$TENANT_HOST/IntegrationDesigntimeArtifacts(Id='$ARTIFACT_ID',Version='$ARTIFACT_VERSION')/"'$value'
-mkdir $FOLDER 2> /dev/null
-unzip -d $FOLDER $ZIP_FILE_NAME
-rm $ZIP_FILE_NAME
+     -o "$ZIP_FILE_NAME" \
+     -w %{http_code} \
+     "$TENANT_HOST/IntegrationDesigntimeArtifacts(Id='$ARTIFACT_ID',Version='$ARTIFACT_VERSION')/"'$value')
+if [ $RESPONSE == "200" ]; then
+  mkdir "$FOLDER" 2> /dev/null
+  unzip -d "$FOLDER" "$ZIP_FILE_NAME"
+else
+  printf "Error downloading %s: %s\n" "$ARTIFACT_ID" "$RESPONSE" 1>&2
+fi
+  rm "$ZIP_FILE_NAME"
--- configure.sh
+++ configure.sh
@@ -9,7 +9,20 @@ CONFIG_FILE=.config
 if [ -f $CONFIG_FILE ]; then
   . ./$CONFIG_FILE
 fi
+
+if [ "$1" == "-a" ]; then
+  ACCOUNT_ID=$2
+  shift 2
+fi
+
+print_options() {
+  printf "Options: -a <account_id>\n" 1>&2
+  for var in "$@"; do
+    printf "\t $var\n" 1>&2
+  done
+}
 save_config() {
-  printf "USER=%s\n" $USER > $CONFIG_FILE
+  printf "ACCOUNT_ID=%s\n" $ACCOUNT_ID > $CONFIG_FILE
+  printf "USER=%s\n" $USER >> $CONFIG_FILE
   printf "XCSRFTOKEN=%s\n" $XCSRFTOKEN >> $CONFIG_FILE
 }
