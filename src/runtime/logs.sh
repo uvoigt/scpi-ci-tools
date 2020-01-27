d270402c4296c68fb0e10d36ee7956e6539129e7
Uwe Voigt <uwe.voigt@smartcommerce.de>
2020-01-27 17:18:37 +0100

    workaround for buggy API

--- src/runtime/logs.sh
+++ src/runtime/logs.sh
@@ -7,10 +7,12 @@
 
 . configure.sh
 
-execute_api_request_with_retry "api/v1/LogFiles"
+# %24orderby=LastModified&%24top=1   the API is buggy, no sort no skip or top
+# therefore order with jq
+execute_api_request_with_retry "api/v1/LogFiles?%24filter=LogFileType%20eq%20'trace'"
 tmpFile=$TMPDIR/_log.gz
 if [ "$RESPONSE_CODE" = 200 ]; then
-  read -r name application <<< $(jq -r '.d.results | last | .Name, .Application' <<< "$RESPONSE")
+  read -r name application <<< $(jq -r '.d | .results |= sort_by(.LastModified) | .results | last | .Name, .Application' <<< "$RESPONSE")
   execute_api_request_with_retry \
     "api/v1/LogFiles(Name='$name',Application='$application')/%24value" \
     GET \
